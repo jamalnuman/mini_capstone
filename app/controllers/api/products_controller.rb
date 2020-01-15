bcff291c148dd6237fae63703bb64059d3ee96e7
“YOUR <jnuman82@gmail.com>
2020-01-15 11:03:30 -0600

    routes for products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,4 +5,14 @@ class Api::ProductsController < ApplicationController
     render "products.json.jb"
   end
 
+  def first_product
+    @first_product = Product.first
+    render "first_product.json.jb"
+  end
+
+  def second_product
+    @second_product = Product.second
+    render "second_product.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
 
     get "/all_products" => "products#all_product"
+    get "/first_product" => "products#first_product"
+    get "/second_product" => "products#second_product"
     
   end
 end
