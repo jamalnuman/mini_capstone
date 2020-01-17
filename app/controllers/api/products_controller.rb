3cda8686a44e79619cc97cbf403bee20055b6950
“jamalnuman” <jnuman82@gmail.com>
2020-01-17 11:35:20 -0600

    daily update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,18 +1,45 @@
 class Api::ProductsController < ApplicationController
 
-  def all_product
-    @products = Product.all
-    render "products.json.jb"
+  def index 
+    @products = Product.all 
+    render 'index.json.jb'
   end
 
-  def first_product
-    @first_product = Product.first
-    render "first_product.json.jb"
+  def show
+    @product = Product.find(params[:id]) #remember this returns a string, but under the hood it will be converted to a number.
+    render 'show.json.jb'
   end
 
-  def second_product
-    @second_product = Product.second
-    render "second_product.json.jb"
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    
+    @product.save
+    render 'show.json.jb'
+
+  end 
+
+  def create
+    @product = Product.new(
+                            id: params[:id],
+                            name: params[:name],
+                            price: params[:price],
+                            description: params[:description],
+                            image_url: params[:image_url] #in order for these tags to show in insomnia or any other app, they must be here first 
+                            )
+    @product.save
+    render 'show.json.jb'
+
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Product #{product.id} has successfully been destroyed."}
   end
 
 end
