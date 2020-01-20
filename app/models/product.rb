0f93ce3a0796e9c23f2f0afb953225807b70abc0
“jamalnuman” <jnuman82@gmail.com>
2020-01-20 12:04:57 -0600

    partial file update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    # discounted = price < 10 ? true : false
+    # discounted
+    price < 10
+  end
+
+  def tax
+    price * 0.09
+  end 
+
+  def total
+    price + tax
+  end
 end
